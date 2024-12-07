fcd41188dd8e8cf31a171de54a78f329374c9a5d
Czasku <t835317@gmail.com>
2024-12-07 17:52:12 +0100

    Symulacja
    dodanie symulacji

--- Projekt-PK_sem3-4/Symulacja.cpp
+++ Projekt-PK_sem3-4/Symulacja.cpp
@@ -4,3 +4,26 @@
 #include "Sprzezenie_zwrotne.h"
 #include "Symulacja.h"
 #include "Zarzadzanie_plikami.h"
+
+
+Symulacja::Symulacja(PID pid, ARX arx, Generator gen, int liczbaKrokow_)
+    : regulator(pid), obiekt(arx), generator(gen), liczbaKrokow(liczbaKrokow_)
+{}
+
+void Symulacja::uruchom() {
+    float y = 0.0f;
+    float w = 0.0f;
+    float u = 0.0f;
+    float e = 0.0f;
+
+    regulator.Reset();
+
+    for (int i = 0; i < liczbaKrokow; i++) {
+        w = generator.Generuj(i);
+        e = regulator.Sumator(w, y);
+        u = regulator.ObliczSterowanie(e);
+        y = obiekt.Oblicz(u);
+
+        cout << "Wejscie:\t" << w << " PID:\t" << e << " \tWyjscie:\t" << y << endl;
+    }
+}
--- Projekt-PK_sem3-4/Symulacja.h
+++ Projekt-PK_sem3-4/Symulacja.h
@@ -1,6 +1,5 @@
 #pragma once
-#include "glowne.h"
-
+/*
 class Symulacja;
 
 class Symulacja
@@ -9,4 +8,41 @@ class Symulacja
 public:
 	void StartSymulacji();
 	void ResetSymulacji();
+};
+*/
+#include "glowne.h"
+
+#include "PID.h"
+#include "ARX.h"
+#include "Generator.h"
+
+class Symulacja {
+private:
+    PID regulator;      // Kompozycja: obiekt PID
+    ARX obiekt;         // Kompozycja: obiekt ARX
+    Generator generator; // Kompozycja: obiekt Generator
+    int liczbaKrokow;
+
+    // Wektory do przechowywania wyników symulacji
+    std::vector<float> czasy;
+    std::vector<float> wyjscia;
+    std::vector<float> zadane;
+    std::vector<float> sterowania;
+
+public:
+    // Konstruktor przyjmuje obiekty PID, ARX i Generator przez const&, 
+    // nastêpnie kopiuje je do w³asnych pól.
+    Symulacja(PID pid, ARX arx, Generator gen, int liczbaKrokow_);
+
+    void uruchom();  // Metoda uruchamiaj¹ca pêtlê symulacji
+
+    // Metody do pobrania wyników po symulacji
+    const std::vector<float>& pobierzCzasy() const { return czasy; }
+    const std::vector<float>& pobierzWyjscia() const { return wyjscia; }
+    const std::vector<float>& pobierzZadane() const { return zadane; }
+    const std::vector<float>& pobierzSterowania() const { return sterowania; }
+
+    // Metody do zapisu i odczytu konfiguracji do/z pliku tekstowego
+    void ZapiszKonfiguracje(const std::string& nazwaPliku) const;
+    void WczytajKonfiguracje(const std::string& nazwaPliku);
 };
